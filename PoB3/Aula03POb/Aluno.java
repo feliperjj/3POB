c95649d65880256586a833349a10fb675d8df9ef
Felipe Bento <81867174+feliperjj@users.noreply.github.com>
2023-09-21 13:33:21 -0300

    Add files via upload

---
+++ PoB3/Aula03POb/Aluno.java
@@ -0,0 +1,16 @@
+package Aluno;
+
+public class Aluno {
+	private String nome;
+	private String matricula;
+	private String email;
+	private int idade;
+	  public Aluno(String nome, String email, int idade, String matricula) {
+	        this.nome = nome;
+	        this.email = email;
+	        this.idade = idade;
+	        this.matricula = matricula;
+	    }
+	public Aluno(String nome2, String cpf, String dataNasc, String matricula2, String endereco, String cep) {
+		// TODO Auto-generated constructor stub
+	}
---
+++ PoB3/Aula03POb/Main.java
@@ -0,0 +1,14 @@
+package Aluno;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Principal menu = new Principal();
+		menu.Processamento();
+
+	}
+
+}
