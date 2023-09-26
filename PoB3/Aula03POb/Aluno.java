28f92ceaf589355715f5e61b62ab22e16bea8be1
Felipe Bento <81867174+feliperjj@users.noreply.github.com>
2023-09-25 21:45:54 -0300

    Update Aluno.java

--- PoB3/Aula03POb/Aluno.java
+++ PoB3/Aula03POb/Aluno.java
@@ -1,16 +1,75 @@
 package Aluno;
 
+import java.util.Date;
+
 public class Aluno {
-	private String nome;
-	private String matricula;
-	private String email;
-	private int idade;
-	  public Aluno(String nome, String email, int idade, String matricula) {
-	        this.nome = nome;
-	        this.email = email;
-	        this.idade = idade;
-	        this.matricula = matricula;
-	    }
-	public Aluno(String nome2, String cpf, String dataNasc, String matricula2, String endereco, String cep) {
-		// TODO Auto-generated constructor stub
-	}
+    private String nome;
+    private String cpf;
+    private String matricula;
+    private String endereco;
+    private String cep;
+    private String dataNascStr;
+
+    public Aluno(String nome, String cpf, Date dataNasc, String endereco, String cep, String dataNascStr, String matricula) {
+        super();
+        this.nome = nome;
+        this.cpf = cpf;
+        this.matricula = matricula;
+        this.endereco = endereco;
+        this.cep = cep;
+        this.dataNascStr = dataNascStr;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getDataNascStr() {
+        return dataNascStr;
+    }
+
+    public void setDataNascStr(String dataNascStr) {
+        this.dataNascStr = dataNascStr;
+    }
+
+    
+    public String toString() {
+        return "Nome do Aluno: " + nome + "\nMatricula: " + matricula + "\nData de Nascimento: " + dataNascStr + "\nEndereço: " + endereco + "\nCEP: " + cep + "\nCPF: " + cpf;
+    }
+}
